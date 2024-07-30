2c39de6217c2270e0d5a62704e5d9d6c30b4f0a8
Namgyu Yoo <namgyuyoo@rtm.ai>
2024-07-30 17:12:15 +0900

    ERROR

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ APP = ['gui.py']
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['numpy'],  # Include numpy package
-    'excludes': ['numpy.typing.tests'],  # Exclude the problematic test modules
+    'excludes': ['numpy.typing.tests','fcntl'],  # Exclude the problematic test modules
 }
 
 setup(
@@ -12,3 +12,4 @@ setup(
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+
